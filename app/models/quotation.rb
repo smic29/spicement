26e8134bf293ff0c95ecd9a39c7ef8688c873835
smic29 <sibulo1991@gmail.com>
2024-05-13 18:44:57 +0800

    Added validations and tests to quotations

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -3,4 +3,10 @@ class Quotation < ApplicationRecord
   belongs_to :client
   has_many :quote_line_items
   has_many :billings
+
+  validates :user_id, presence: true
+  validates :client_id, presence: true
+  validates :status, inclusion: { in: %w(draft approved rejected) }
+  validates :total_price, numericality: true
+
 end
--- spec/models/quotation_spec.rb
+++ spec/models/quotation_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Quotation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:client) }
+    it { should have_many(:quote_line_items) }
+    it { should have_many(:billings) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:client_id) }
+    it { should validate_inclusion_of(:status).in_array(%w(draft approved rejected)) }
+    it { should validate_numericality_of(:total_price) }
+  end
 end
