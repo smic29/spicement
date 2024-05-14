3fb6b069cf637ca8ccdb8bcaba2b88aa5e9cb8e3
smic29 <sibulo1991@gmail.com>
2024-05-14 15:31:36 +0800

    Added validations and tests for forwarders

--- app/models/forwarder.rb
+++ app/models/forwarder.rb
@@ -2,4 +2,7 @@ class Forwarder < ApplicationRecord
   belongs_to :user
   has_many :people
   has_many :bookings
+
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :name, length: { maximum: 100 }
 end
--- spec/models/forwarder_spec.rb
+++ spec/models/forwarder_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Forwarder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:people) }
+    it { should have_many(:bookings) }
+  end
+
+  describe 'validations' do
+    subject { FactoryBot.build(:forwarder) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(100) }
+    it { should validate_uniqueness_of(:name).scoped_to(:user_id) }
+  end
 end
