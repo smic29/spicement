a78bb8f32331f3cc0e12cd3f8549b31c562f3970
smic29 <sibulo1991@gmail.com>
2024-05-16 00:10:47 +0800

    Configured devise sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   def new
-    @company = Company.find(params[:id])
+    id = params[:id] || params.dig(:user, :company_id)
+    @company = Company.find(id)
     if @company.approved
       super
     else
@@ -14,9 +15,17 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    if resource && resource.active_for_authentication?
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -26,7 +35,7 @@ class Users::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:company_id])
+  end
 end
