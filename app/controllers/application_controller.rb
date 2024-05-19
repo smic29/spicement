9f481cdf5eb1abddb0759ff3cf3b0dc3476a1744
smic29 <sibulo1991@gmail.com>
2024-05-19 18:31:48 +0800

    Added initial controllers and index view for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   def prepend_toast
     turbo_stream.prepend("toast_container", partial: 'shared/alerts')
   end
+
+  private
+
+  def authenticate_admin!
+    authenticate_user!
+    redirect_to root_path, alert: 'You are not authorized to access this page.' unless current_user.admin?
+  end
 end
