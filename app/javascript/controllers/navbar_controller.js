54e084ca4ba1e81f1699629db9a4143edf410e04
smic29 <sibulo1991@gmail.com>
2024-05-20 20:41:44 +0800

    Added stimulus to admin navbar to set current active page

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = [ "links" ]
+  connect() {
+    const turbo_frame = document.querySelector('#admin_frame')
+
+    this.setInitialActive(turbo_frame)
+    this.setEventListener()
+  }
+
+  setInitialActive(frame) {
+    this.linksTargets.forEach((link) => {
+      if (link.href === frame.src || link.textContent === 'Home' ) {
+        link.classList.add('active', 'pe-none')
+      } else {
+        link.classList.remove('active')
+      }
+    })
+  }
+
+  setEventListener() {
+    document.addEventListener('turbo:before-frame-render', (e) => {
+      this.linksTargets.forEach((link) => {
+        if (link.href === e.target.src) {
+          link.classList.add('active', 'pe-none')
+        } else {
+          link.classList.remove('active', 'pe-none')
+        }
+      })
+    })
+  }
+}
