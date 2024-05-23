dabf90b7048350d1f7b96b301c6fe7eadc3663dc
smic29 <sibulo1991@gmail.com>
2024-05-23 22:21:54 +0800

    Added form and inputs for new quotation, and configured permitted params for controller

--- app/controllers/users/quotations_controller.rb
+++ app/controllers/users/quotations_controller.rb
@@ -6,6 +6,15 @@ class Users::QuotationsController < ApplicationController
   end
 
   def create
+    puts quotation_params
+  end
 
+  private
+
+  def quotation_params
+    params.require(:quotation).permit(
+      :status, :exchange_rate, :origin, :destination, :incoterm, client: [:name, :address],
+      quotelineitems: [:description, :currency, :cost, :frequency, :quantity, :total]
+    )
   end
 end
