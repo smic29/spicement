3f1bac68f4b299f3521eff65d6f7c6fa7a1c45d0
smic29 <sibulo1991@gmail.com>
2024-05-27 13:58:38 +0800

    Added a number to currency helper that defaults to PHP symbol

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def number_to_currency_php(number, options = {})
+    options[:unit] ||= '₱'
+    options[:separator] ||= '.'
+    options[:delimiter] ||= ','
+    number_to_currency number, options
+  end
 end
