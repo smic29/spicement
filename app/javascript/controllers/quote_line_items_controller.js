0cc9281ef0975f3988535a11853377a7d86c2755
smic29 <sibulo1991@gmail.com>
2024-05-29 18:44:52 +0800

    Added sum in form view and modified stimulus to update accordingly

--- app/javascript/controllers/quote_line_items_controller.js
+++ app/javascript/controllers/quote_line_items_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="quote-line-items"
 export default class extends Controller {
-  static targets = ["container", "template"]
+  static targets = ["container", "template", "sumtotal"]
 
   initialize() {
     this.indexValue = this.templateTargets.length
@@ -10,8 +10,26 @@ export default class extends Controller {
     this.templateTargets.forEach((template) => {
       if (template.querySelector('input').value !== "") {
         template.querySelector('.d-none').classList.remove('d-none')
+        this.updateSumTotal()
       }
     })
+
+    this.element.addEventListener("change", (e) => {
+      this.updateSumTotal()
+    })
+  }
+
+  updateSumTotal() {
+    let sum = 0
+
+    this.templateTargets.forEach((template) => {
+      const totalEl = template.querySelector('[id*="_total"]')
+
+      sum += parseFloat(totalEl.value === NaN ? 0 : totalEl.value)
+    })
+
+    const formattedSum = sum.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    this.sumtotalTarget.textContent = formattedSum
   }
 
   add(e) {
@@ -43,6 +61,7 @@ export default class extends Controller {
       }
       this.indexValue--
       this.updateIndices(lineItemIndex)
+      this.updateSumTotal()
     }
   }
 
@@ -60,11 +79,25 @@ export default class extends Controller {
         const newIndex = index 
         const namePattern = /name="quotation\[quote_line_items_attributes\]\[\d+\]\[/g
         const idPattern = /id="quotation_quote_line_items_attributes_\d+_/g
+        
+        // Save the current values
+        const current_values = []
+        element.querySelectorAll('input').forEach((input) => {
+          current_values.push(input.value)
+        })
+        const select_value = element.querySelector('select').value
 
         element.innerHTML = element.innerHTML
         .replace(namePattern, `name="quotation[quote_line_items_attributes][${newIndex}][`)
         .replace(idPattern, `id="quotation_quote_line_items_attributes_${newIndex}_`)
 
+        // Apply them after change
+        element.querySelectorAll('input').forEach((input, index) => {
+          input.value = current_values[index]
+        })
+        const selectEl = element.querySelector('select')
+        selectEl.value = select_value
+        
         const nextElement = element.nextElementSibling
         if (nextElement && nextElement.tagName === 'INPUT') {
             nextElement.name = `quotation[quote_line_items_attributes][${newIndex}][id]`
