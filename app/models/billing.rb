89dc0fbc259de42a76a097d2d6df0abe30c24e4c
smic29 <sibulo1991@gmail.com>
2024-06-05 19:30:59 +0800

    Added details section to new billing form

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -5,6 +5,8 @@ class Billing < ApplicationRecord
 
   accepts_nested_attributes_for :quotation, update_only: true
 
+  DOC_TYPES = ['Billing Invoice', 'Statement of Account'].freeze
+
   validates :user, :booking, :quotation, presence: true
   validates :total_amount, :ex_rate, numericality: true, allow_nil: true
   validates :status, inclusion: { in: %w(Draft Sent Paid) }
