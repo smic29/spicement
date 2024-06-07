6342902cf0343ac346f7360e85ef8319dcf6333f
smic29 <sibulo1991@gmail.com>
2024-06-07 16:20:58 +0800

    Added billing params

--- app/controllers/users/billings_controller.rb
+++ app/controllers/users/billings_controller.rb
@@ -22,7 +22,7 @@ class Users::BillingsController < ApplicationController
   end
 
   def create
-    puts 'Triggered'
+    puts 'Entered Create Action'
   end
 
   def edit
@@ -31,5 +31,23 @@ class Users::BillingsController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def billing_params
+    params.require(:billing).permit(
+      :booking_id, :quotation_id, :type, :status, :ex_rate,
+      quotation_attributes: [
+        :origin, :destination, :id,
+        client_attributes: [:name, :address, :tin_number, :id]
+      ],
+      booking_attributes: [
+        :vessel_info, :bl_number, :eta, :cargo_volume, :services, :id
+      ],
+      billing_line_items_attributes: [
+        :description, :currency, :cost, :quantity, :total
+      ]
+    )
   end
 end
