a7cd1215ea63395a403cf12cbef3c36d7318f0c3
smic29 <sibulo1991@gmail.com>
2024-07-15 13:06:55 +0800

    Updated number in to words helped for billing pdf

--- app/helpers/users/billings_helper.rb
+++ app/helpers/users/billings_helper.rb
@@ -2,14 +2,12 @@ module Users::BillingsHelper
   def convert_to_words(num)
 
     whole_num_part = num.to_i
-    decimal_part = num.frac
-
-    fraction = decimal_part.to_f.rationalize
+    decimal_part = (num - whole_num_part).round(2)
 
     whole_number_words = I18n.with_locale(:en) { whole_num_part.to_words hundreds_with_union: true }
 
-    numerator = fraction.numerator
-    denominator = fraction.denominator
+    numerator = (decimal_part * 100).to_i
+    denominator = 100
     fraction_words = "#{numerator}/#{denominator}"
 
     result = "#{whole_number_words} and #{fraction_words} pesos only"
